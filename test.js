ea79358a0549524223c8b9a641c59cbff8536e3d
obre-bjorn <obre.bjorn@gmail.com>
2023-02-25 14:21:13 +0300

    Check Winner logic

--- test.js
+++ test.js
@@ -1,8 +1,36 @@
 let array = [
-    [1, 1, 1],
-    [2, 3, 5],
-    [5, 6, 7]
+    [1, 3, 1],
+    [1, 3, 5],
+    [6, 3, 6]
 ]
 
 
-console.log(array[0].every(number => number === 1))
+// console.log(array.every((number) => {
+//     number[0] === 1
+// }))
+
+
+function checkWinner(board) {
+    result = false
+
+    board.forEach((row, index) => {
+        // check rows
+        if (row.every(token => token === row[index])) {
+            result = true;
+            return
+        }
+        // Check Columns
+        if (row.every(token => token[0] === row[index][0])) {
+            result = true;
+            return
+        }
+        //Check Diagonals
+        if (row.every)
+
+    });
+
+    //Check columns
+
+    return result
+}
+console.log(checkWinner(array))
