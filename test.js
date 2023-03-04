98c3bc080e84f8d5e9e9d28bc2e63902991f18c6
obre-bjorn <obre.bjorn@gmail.com>
2023-03-04 12:16:06 +0300

    test random pick

--- test.js
+++ test.js
@@ -1,23 +1,62 @@
-// 
+let board = [
+    ['X', '', ''],
+    ['', '', ''],
+    ['X', '', '']
+]
 
 
-function Human() {
+function checkAvailableCells(board) {
+    let availableCells = []
 
-    function sayName(name) {
-        console.log(`My name is ${name}`)
+    for (let row = 0; row < board.length; row++) {
+        for (let cell = 0; cell < board[row].length; cell++) {
+            // console.log(board[row][cell])
+            if (!board[row][cell]) availableCells.push(row + '' + cell)
+        }
     }
 
-    function eat() {
-        console.log('eat')
-    }
-
-    return {
-        sayName,
-        eat
-    }
-
-};
+    return availableCells
+}
 
 
-Human().sayName('bjorn')
-Human().eat()
+function computerPlay(board) {
+    let availbaleSelections = checkAvailableCells(board)
+    let computerPick = Math.floor(Math.random(0) * availbaleSelections.length)
+    computerPick = availbaleSelections[computerPick]
+    let [row, column] = computerPick.split('')
+    board[row][column] = 'O'
+    return board
+}
+
+
+
+console.table(computerPlay(board))
+console.table(computerPlay(board))
+console.table(computerPlay(board))
+console.table(computerPlay(board))
+    // 
+
+
+// function Human() {
+
+//     function sayName(name) {
+//         console.log(`My name is ${name}`)
+//     }
+
+//     function eat() {
+//         console.log('eat')
+//     }
+
+//     return {
+//         sayName,
+//         eat
+//     }
+
+// };
+
+
+// Human().sayName('bjorn')
+// Human().eat()me('bjorn')
+// Human().eat()me('bjorn')
+// Human().eat()me('bjorn')
+// Human().eat()
